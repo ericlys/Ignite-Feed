e2898a2f01f408e277bb4dece03598522b7ec0d8
Ericlys Moreira <ericlysm@gmail.com>
2022-07-11 08:17:19 -0300

    feat: add sidebar

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+        <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium amet adipisci blanditiis beatae non ratione asperiores, libero quod, perspiciatis eius voluptas iusto voluptatum maxime. Delectus quia corrupti in asperiores earum." 
+        />
+        <Post 
+          author="Ericlys" 
+          content="Um novo post muito legal." 
+        />
+        </main>
+      </div>
+    </div>
+  )
+}
