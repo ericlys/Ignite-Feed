18d5037c9e92507c65b97c1bf5ac60ef8ea843c4
Ericlys Moreira <ericlysm@gmail.com>
2022-07-24 11:09:17 -0300

    feat: comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return(
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+           <div className={styles.authorAndTime}>
+            <strong>Ericlys Moreira</strong>
+            <time title='11 de julho ás 8:00h' dateTime='2022-07-11 08:00:00'>Cerca de 1h atrás</time>
+           </div>
+
+           <button title='Deletar comentário' >
+            <Trash size={24} />
+           </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👋👋</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
