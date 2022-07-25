ec7ac3320c08ebe109affbba4c4e192e371412c5
Ericlys Moreira <ericlysm@gmail.com>
2022-07-25 11:15:55 -0300

    Iterating in JSX, post page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import styles from './App.module.css';
 
 import './global.css';
 
+//author: {avatar_url: "", name: "", role: ""}
+//publishedAt: Date
+//content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/ericlys.png",
+      name: "Ericlys Moreira",
+      role: "Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}     
+    ],
+    publishedAt: new Date('2022-07-25 11:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}     
+    ],
+    publishedAt: new Date('2022-07-25 11:00:00')
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -14,14 +49,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-        <Post 
-          author="Diego Fernandes" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium amet adipisci blanditiis beatae non ratione asperiores, libero quod, perspiciatis eius voluptas iusto voluptatum maxime. Delectus quia corrupti in asperiores earum." 
-        />
-        <Post 
-          author="Diego Fernandes" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium amet adipisci blanditiis beatae non ratione asperiores, libero quod, perspiciatis eius voluptas iusto voluptatum maxime. Delectus quia corrupti in asperiores earum." 
-        />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
