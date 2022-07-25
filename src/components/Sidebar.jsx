8f9efcf2ac953354087127915773726bb8f6baa5
Ericlys Moreira <ericlysm@gmail.com>
2022-07-25 09:49:23 -0300

    feat: avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,8 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/30418565?v=4'/>
-
+        <Avatar src="https://avatars.githubusercontent.com/u/30418565?v=4" />
         <strong>Ericlys Moreira</strong>
         <span>Web Developer</span>
       </div>
