e75cc7051582e0d3c27cfd0cd4c8ff6499160ccf
Ericlys Moreira <ericlysm@gmail.com>
2022-07-31 15:35:37 -0300

    feat: adding typescript to the project

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,7 +3,12 @@ import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content, onDeleteComment }) {
+interface ComentProps{
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: ComentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,27 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'; 
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post({ author, publishedAt, content }) {
+interface Content {
+  type: 'paragraph' | 'link';
+  publishedAt: Date;
+  content: string;
+}
+
+interface PostProps {
+  author: {
+    name: string;
+    role: string;
+    avatarUrl: string;
+  }
+  publishedAt: Date;
+  content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState([
     'Post muito bacana, hein?!'
   ]);
@@ -20,23 +36,23 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true
   })
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText('');
   }
 
-  function handleNewCommentChange() {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
   }
 
-  function handleNewCommentInvalid() {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
    event.target.setCustomValidity('Esse campo é obrigatório');
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentWithoutDeleteOne = comments.filter( comment => {
       return comment !== commentToDelete;
     })
